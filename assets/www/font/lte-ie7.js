b09b821a0a131b96e344a5e73be40715dd2db122
Miguel Veces <mveces@yahoo.com>
2013-05-06 22:07:30 -0500

    #4 agregando estilos a las pantallas commit #2 Signed-off-by:Miguel Veces <mveces@yahoo.com>

---
+++ assets/www/font/lte-ie7.js
@@ -0,0 +1,37 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-film' : '&#xe000;',
+			'icon-play' : '&#xe001;',
+			'icon-pause' : '&#xe002;',
+			'icon-play-2' : '&#xe003;',
+			'icon-cart' : '&#xe004;',
+			'icon-bolt' : '&#xe005;',
+			'icon-wand' : '&#xe006;',
+			'icon-contract' : '&#xe007;',
+			'icon-barcode' : '&#xe008;',
+			'icon-expand' : '&#xe009;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
