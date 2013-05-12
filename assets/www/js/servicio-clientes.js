68a6fe0b8d5568d7003ff3ca1be3943ec4527695
Euclides Arboleda <euclides_arboleda@hotmail.com>
2013-05-12 13:06:41 -0500

    se agrega la logica para usar el web services

--- assets/www/js/servicio-clientes.js
+++ assets/www/js/servicio-clientes.js
@@ -8,16 +8,18 @@ function LoginButton_onclick() {
         type: "POST",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        url: "http://190.72.192.18/webservicedemo/service.asmx/HelloWorld",
+//        url: "http://190.72.192.18/webservicedemo/service.asmx/HelloWorld",
+        url: "http://bancoservicios.somee.com/BancoDePuntos/Business_Services/App_Services/WSCuenta.asmx/HelloWorld",
         data: '{}',
         success: function(msg) {
             jsonArray = $.parseJSON(msg.d);
             var $ul = $('<ul id="details">');
             for (i = 0; i < jsonArray.length; i++)
             {
-                $("#details").append('<li id="' + i + '" name="head" >' + jsonArray[i].name + '</li>');
+                $("#details").append('<li id="' + i + '" name="head" >' + jsonArray[i].Nombre + '</li>');
             }
-            $('#details').listview('refresh');
+//            $('#details').listview('refresh');
+            alert("redirecionar");
         },
         error: function(msg) {
             alert("Error" + msg);
